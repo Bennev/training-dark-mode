c5d814475914c205d7334b68ba55a968ecad20e7
Matheus Benevides Militão <mb.militao@gmail.com>
2022-09-01 00:33:17 -0300

    update logo main

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -3,8 +3,10 @@ import "./style.css"
 import React from "react"
 
 export default function Main(props) {
+
     return (
-        <main className={props.darkMode ? "dark" : ""}>
+        <main style={props.darkMode ? {backgroundImage: "url(/images/react-icon-dark.png)"} : {backgroundImage: "url(/images/react-icon-light.png)"}} 
+        className={props.darkMode ? "dark" : ""}>
             <h1 className="main--title">Fun facts about React</h1>
             <ul className="main--facts">
                 <li>Was first released in 2013</li>
