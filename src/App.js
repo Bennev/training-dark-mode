39a4cb0d0a44c40b1baed00f70f9abaf18a88dd8
Matheus Benevides Militão <mb.militao@gmail.com>
2022-10-03 22:33:14 -0300

    update readme

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import React, {useState} from "react"
 import Navbar from "./components/Navbar"
 import Main from "./components/Main"
+import Footer from "./components/Footer"
 
 function App() {
   const [darkMode, setDarkMode] = useState(true)
@@ -14,6 +15,7 @@ function App() {
           toggleDarkMode={() => setDarkMode(prevMode => !prevMode)}
       />
       <Main darkMode={darkMode} />
+      <Footer darkMode={darkMode} />
     </div>
   );
 }
